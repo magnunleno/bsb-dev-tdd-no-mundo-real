2d355b822990b1d838b7bff9529d733678749462
Magnun Leno <magnun.leno@gmail.com>
2018-11-04 23:37:56 -0200

    Esqueleto do décimo segundo teste

--- projeto/tests/test_valor_compra.py
+++ projeto/tests/test_valor_compra.py
@@ -21,3 +21,18 @@ def test_valor_compra(client, fake_users, fake_produtos, fake_compras):
         response = client.get(url)
         assert response.status_code == 200
         assert response.data['valor_total'] == valor_total
+
+
+def test_valor_compra_apos_alteracao_produto(client, fake_users, fake_produtos,
+                                             fake_compras):
+    '''
+    Valida que o valor da compra não se altera após a alteração do valor de um
+    ou mais produtos.
+    '''
+    user = fake_users()
+    compra = fake_compras(comprador=user, produtos=fake_produtos(3))
+    valor_total = sum([produto.valor for produto in compra.produtos.all()])
+    url = reverse('compras-detail', args=[compra.pk])
+
+    with client.auth(user=user):
+        ...
